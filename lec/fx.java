5ac5c1082855fc674b7b523505be1ebdb1f81e0a
sulafa009 <mmontaha447@gmail.com>
2023-06-18 14:09:02 +0300

    create new project

---
+++ lec/fx.java
@@ -0,0 +1,19 @@
+
+
+
+
+public static 	void main (String args[]) {
+	
+	System.out.println(num(9.1 ,1.9));
+	
+}
+
+public static  double num ( double s,double m ) {
+	
+	return s+m ;
+	
+	
+	
+}
+					
+}
